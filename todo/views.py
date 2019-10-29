ac713fa02fc9538b17255a01501477306a3a460a
David <haydal.tmps@gmail.com>
2019-10-29 22:59:24 +0000

    render html page & display data from sqlite table

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from .models import Item
 
 # Create your views here.
 
-def get_todo_list(request):
-    return render(request, "todo_list.html")
 
-    
+def get_todo_list(request):
+    results = Item.objects.all()
+    return render(request, "todo_list.html", {'item': results})
