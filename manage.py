efa516d12a02d294f6d8feabe7395735162c6848
David <haydal.tmps@gmail.com>
2019-11-01 20:49:43 +0000

    same name of folder change to manage.py

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj_project.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_todo.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
